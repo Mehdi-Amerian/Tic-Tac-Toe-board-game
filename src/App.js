639fc142176be9ba13792058df330a50b79ae458
El Mahdi Amerian <mehdiamerian@gmail.com>
2024-01-27 22:07:26 +0200

    Basic styling

--- src/App.js
+++ src/App.js
@@ -119,5 +119,16 @@ export default function Game() {
     );
   });
 
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+
  
 }
