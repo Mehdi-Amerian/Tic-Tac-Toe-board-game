22b2c45575f557b929bbe32809cfb7670d436c50
El Mahdi Amerian <mehdiamerian@gmail.com>
2024-01-27 20:14:32 +0200

    Initial setup and square component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
-export default function Square() {
-  return <button className="square">X</button>;
+// Importing the 'useState' hook from React
+import { useState } from 'react';
+
+// Square component representing an individual square in the game
+function Square({ value, onSquareClick }) {
+  return (
+    // Button jsx element representing the square (<button>), with a CSS class(className="square") 
+    //and an onClick event handler(onClick={onSquareClick})
+    <button className="square" onClick={onSquareClick}>
+      {value} {/* Displaying the value ('X', 'O', or null) inside the button */}
+    </button>
+  );
 }
